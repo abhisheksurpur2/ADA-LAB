ae4908ffcc5b09ed6005d8fdb28fecadbb88f0c6
AbhishekH1936 <53899135+AbhishekH1936@users.noreply.github.com>
2019-09-26 15:24:01 +0530

    Add files via upload

---
+++ nqueen.py
@@ -0,0 +1,59 @@
+global N 
+N=4
+ 
+def printx(board): 
+    for i in range(N): 
+        for j in range(N): 
+            print (board[i][j], end = " ") 
+        print()
+
+def check_isSafe(board, row, col): 
+      
+    for i in range(col): 
+        if board[row][i] == 1: 
+            return False
+
+    for i, j in zip(range(row, -1, -1),
+                    range(col, -1, -1)): 
+        if board[i][j] == 1: 
+            return False
+  
+    for i, j in zip(range(row, N, 1),  
+                    range(col, -1, -1)): 
+        if board[i][j] == 1: 
+            return False
+  
+    return True
+
+
+def solveNQ(board, col): 
+    
+    if col >= N: 
+        return True
+  
+    
+    for i in range(N): 
+  
+        if check_isSafe(board, i, col): 
+              
+            
+            board[i][col] = 1
+  
+            
+            if solveNQ(board, col + 1) == True: 
+                return True
+  
+            
+            board[i][col] = 0
+  
+   
+    return False
+  
+grid=[[0 for x in range(4)]for y in range(4)]
+print(grid)
+
+if solveNQ(grid,0)==False:
+     print('no sol')
+else:
+     printx(grid)
+
