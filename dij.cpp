dd6a346a5dff052e0ad0d34b15088b34a1f64765
AbhishekH1936 <53899135+AbhishekH1936@users.noreply.github.com>
2019-11-23 12:34:22 +0530

    Add files via upload

--- dij.cpp
+++ dij.cpp
@@ -1,35 +1,69 @@
 #include<iostream>
-#define V 5
+
 using namespace std;
-int main(){
-    int graph[V][V] = {{9999,5,9999,9999,9999},{9999,9999,3,2,9999},{4,9999,9999,9999,9999},{9999,2,9999,9999,6},{5,9999,9999,9999,9999}};
-    int src = 0;
-    int dist[V];
-    int vis[V];
-    for(int i=0;i<V;i++){
-        dist[i] = graph[src][i];
-        vis[i] = 0;
-    }
-    int count = 1;
-    dist[0] = 0;
-    cout<<endl;
-    while(count!=V){
-        int min = 9999;
-        int u;
-        for(int j=0;j<V;j++){
-            if(dist[j]<min && vis[j]!=1){
-                min = dist[j];
-                u = j;  
-            }
-        }
-        vis[u] = 1;
-        count++;
-        for(int j=0;j<V;j++){
-            if(min+graph[u][j]<dist[j])
-                dist[j] = min+graph[u][j];
-        }
-    }
-    cout<<"Shortest ditance is: "<<endl;
-    for(int j=0;j<V;j++)
-        cout<<src<<" "<<j<<" "<<dist[j]<<endl;
+
+int main()
+{
+	int n,mat[30][30],src,dist[10],vis[10],count = 0,min,prev[10]={0},u;	
+	cout<<"Enter the no of nodes"<<endl;
+	cin>>n;
+    cout<<"Enter the distance matrix:"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cin>>mat[i][j];
+		}
+	}
+	cout<<"enter the source"<<endl;
+	cin>>src;
+	for(int j=0;j<n;j++)
+		dist[j] = mat[src][j];
+	for(int j=0;j<n;j++)
+		vis[j] = 0;
+	dist[src]=0;
+	vis[src] = 1;
+	count = 1;
+	
+	while(count!=n)
+	{
+		min = 999;
+		for(int i=0;i<n;i++)
+			if(dist[i]<min && vis[i]!=1)
+			{
+				min = dist[i];
+				u = i;
+			}
+		vis[u]=1;
+		count++;
+		
+		for(int i=0;i<n;i++)
+		{
+			if(min+mat[u][i]<dist[i] && vis[i]!=1)
+			{				
+				dist[i] = min+mat[u][i];
+				prev[i] = u;
+			}	
+		}
+	}
+
+	for(int i=0;i<n;i++)
+		cout<<"source "<<src<<" vertex "<<i<<" distance "<<dist[i]<<endl;
+	
+	for(int i =1;i<n;i++)
+	{
+		cout<<"Path from source to vertex "<<i<<endl;
+		int k =0;
+		int j =i;
+		while(j!=k)
+		{	
+			cout<<j<<"<--";
+			j = prev[j];			
+		}
+		cout<<0;
+		cout<<endl;
+	}
+
+
+
 }
